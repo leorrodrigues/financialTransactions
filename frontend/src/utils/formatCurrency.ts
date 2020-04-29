058961c6e123ea518e2559eb2cf9faba90d16211
leonardo rodrigues <leu_rr1@hotmail.com>
2020-04-29 19:19:55 -0300

    goFinance backend only

--- frontend/src/utils/formatCurrency.ts
+++
@@ -1,9 +0,0 @@
-const formatCurrency = (value: number, type: string): string => {
-    const valueWithSignal = type === 'income' ? value : -value;
-    return new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-    }).format(valueWithSignal);
-};
-
-export default formatCurrency;
--- frontend/src/utils/formatDate.ts
+++
@@ -1,7 +0,0 @@
-import Moment from 'moment';
-
-const formatDate = (date: Date): string => {
-    return Moment(date).format('MM/DD/YYYY');
-};
-
-export default formatDate;
